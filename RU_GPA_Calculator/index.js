a45fbfc9f1669d2b7884c4c53b871c18cf774067
Gohar Shinwari <121142075+GoharShinwari@users.noreply.github.com>
2025-01-04 16:37:37 -0500

    Update index.js

--- RU_GPA_Calculator/index.js
+++ RU_GPA_Calculator/index.js
@@ -159,20 +159,16 @@ document.addEventListener('DOMContentLoaded', () => {
         totalCreditsTd.textContent = `${totalCredits}cr`;
         totalPointsTd.textContent = `${totalPoints.toFixed(1)}pts`;
         resultDiv.textContent = `Your new GPA is ${newGPA}`;
-    });
+    }); 
 
     function getGradePoint(grade) {
         const gradePoints = {
             'A+': 4.0,
             'A': 4.0,
-            'A-': 3.7,
             'B+': 3.3,
             'B': 3.0,
-            'B-': 2.7,
             'C+': 2.3,
             'C': 2.0,
-            'C-': 1.7,
-            'D+': 1.3,
             'D': 1.0,
             'F': 0.0
         };
