6598f41303c937722c72eba81eb907403f79fab9
Gohar Shinwari <121142075+GoharShinwari@users.noreply.github.com>
2025-01-06 21:25:23 -0500

    real time button click + track users

---
+++ interactive_clicker/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+counter = 0
+curr_users = 0
+
+@app.route('/')
+def index():
+    return render_template('index.html', counter=counter)
+
+@socketio.on('increment')  
+def handle_increment():
+    global counter
+    counter += 1
+    emit('update_counter', {'counter': counter}, broadcast=True)
+
+@socketio.on('connect')
+def increment_users():
+    global curr_users
+    curr_users += 1 
+    emit('increment_users', {'curr_users': curr_users}, broadcast=True)
+    print(f'User connected. Current users: {curr_users}')
+
+
+@socketio.on('disconnect')
+def decerement_users():
+    global curr_users
+    curr_users += 1 
+    emit('decerement_users', {'curr_users': curr_users}, broadcast=True)
+    print(f'User disconnected. Current users: {curr_users}')
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
